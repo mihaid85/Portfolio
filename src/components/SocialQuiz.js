fe2703ded7530107b30e404f78b3fd84d70a4a8a
Danet Mihai <mihaidanet@gmail.com>
2018-05-07 11:36:43 +0300

    small changes

--- src/components/SocialQuiz.js
+++ src/components/SocialQuiz.js
@@ -1,6 +1,5 @@
 import React from "react";
 import axios from 'axios';
-import { CSSTransitionGroup } from 'react-transition-group';
 import Header from './Header';
 // import styles from "../styles/components/_quizContainer.scss";
 
